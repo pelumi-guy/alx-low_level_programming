4a8e4c6284738649a7abc3e5724fb4da0f703fa7
Pelumi <pelumi.olalekan.g@gmail.com>
2022-06-30 17:00:54 -0700

    Redid Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  *main - entry point
  *Return: Always 0 (success)
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
