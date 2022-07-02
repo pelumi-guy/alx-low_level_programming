9b0c26dbe47588d1f88311267d52bca1ab03693d
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-01 18:07:38 -0700

    Betty styled Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 /**
  *main - main block
- *Description: Print alphabet in lowercase and then in uppercase, 
+ *Description: Print alphabet in lowercase and then in uppercase,
  *followed by a new line
  * Return: 0
  **/
@@ -10,19 +10,19 @@ int main(void)
 {
 	char c = 'a';
 	char A = 'A';
-
+	
 	while (c <= 'z')
 	{
 		putchar(c);
 		c++;
 	}
-
+	
 	while (A <= 'Z')
         {
-                putchar(A);
-                A++;
-        }
-
+        	putchar(A);
+              	A++;
+      	}
+	
 	putchar('\n');
 	return (0);
 }
