da9de424c3ee283ffd7349bf44ca1d829cabc484
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-01 19:49:49 -0700

    Task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - main block
+ *Description: Print all possible combinations of single-digit numbers.
+ * Return: 0
+ **/
+int main(void)
+{
+	int dig1;
+	int dig2;
+
+	for (dig1 = 0; dig1 <= 9; dig1++)
+	{	
+		for (dig2 = 0; dig2 <= 9; dig2++) 
+		{
+			if (dig1 != dig2 && dig1 < dig2)
+			{
+			putchar(48 + dig1);
+			putchar(48 + dig2);
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+
+
+	putchar('\n');
+	return (0);
+}
