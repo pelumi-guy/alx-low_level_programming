673e3d5b36e3b535414b65a02b32b2f82521b84e
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-01 18:22:09 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - main block
+ *Description: Print the alphabet in lowercase except q and e,
+ *followed by a new line
+ * Return: 0
+ **/
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+			c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
