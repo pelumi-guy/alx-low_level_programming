cbe6f742ea90ebba636e25c570dc3f03fc6b1823
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-05 16:48:13 -0700

    Redid Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,11 +4,19 @@
  * Return: 0
  */
 
-#include <stdio.h>
+#include "main.h"
 
-int main()
+int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar("\n");
 
         return(0);
 }
