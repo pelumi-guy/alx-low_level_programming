d9622b8028c45aa5bfc5abc55f7074c4c58aa034
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-05 20:41:15 -0700

    Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function that prints
+ * every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59.
+ *
+ * .
+ * Return: 0 (Success)
+ */
+
+
+void jack_bauer(void)
+{
+	int hr, min, h1, h2, m1, m2;
+	
+	for (hr = 0; hr < 24; hr++)
+	{
+		h1 = h / 10;
+		h2 = h % 10;
+		for (min = 0; min < 60; min++)
+		{
+			m1 = min / 10;
+			m2 = min % 10;
+			_putchar(h1);
+			_putchar(h2);
+			_putchar(:);
+			_putchar(m1);
+			_putchar(m2);
+			_putchar('\n');
+
+		}
+	}
+
+	
+
+	return (0);
+}
