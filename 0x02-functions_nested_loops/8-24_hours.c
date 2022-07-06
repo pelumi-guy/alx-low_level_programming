66e56377f12fecff69beb04d9a9870f6e4b77e9d
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-05 20:54:41 -0700

    Redid Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -22,11 +22,12 @@ void jack_bauer(void)
 		{
 			m1 = min / 10;
 			m2 = min % 10;
-			_putchar(h1);
-			_putchar(h2);
+
+			_putchar(h1 + '0');
+			_putchar(h2 + '0');
 			_putchar(':');
-			_putchar(m1);
-			_putchar(m2);
+			_putchar(m1 + '0');
+			_putchar(m2 + '0');
 			_putchar('\n');
 
 		}
