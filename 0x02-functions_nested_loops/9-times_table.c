7a280bc939e119fe039a1cdeae8700d74e482b24
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-05 21:39:25 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * main - main block
+ * Description:
+ * Return: 0
+ */
+
+
+void times_table(void)
+{
+	int row, col, dig1, dig2, num;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (col = 0; col <= 9; col++)
+		{
+			num = row * col;
+			if (num < 10)
+			{
+				_putchar(num + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');		
+			}
+			else 
+			{
+				dig1 = num / 10;
+				dig2 = num % 10;
+				_putchar(dig1 + '0');
+				_putchar(dig2 + '0');
+				_putchar(',');
+				_putchar(' ');
+
+			}
+			_putchar('\n');
+		}
+	}
+}
