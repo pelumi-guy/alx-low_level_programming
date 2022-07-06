ae1baf0ebf201b39223eec7bfa2479a1453d1c6c
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-05 18:53:29 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * islower - a function that checks for lowercase character.
+ *
+ * Return: Always 0.
+ **/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
+
