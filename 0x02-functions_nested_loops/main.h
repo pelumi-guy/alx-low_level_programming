87936541edb3658c02c32d044a5e2dd6f28f83b3
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-05 17:42:42 -0700

    Redid Task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
 #include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
@@ -5,8 +8,9 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  **/
-
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+#endif
