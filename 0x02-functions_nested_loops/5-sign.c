04ee5dbb8f8fad74da2267de1588bc2caac01227
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-05 19:17:07 -0700

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print sign - prints the sign of a number.
+ * @n: integer variable to be checked.
+ * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n less less than 0.
+ */
+
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar ('0');
+		return (0);
+
+	}
+	else
+	{
+		_putchar ('-');
+		return (-1);
+
+	}
+}
