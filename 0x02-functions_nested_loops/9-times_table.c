f2d852a131dd7a615749798971608b85ed9fb734
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-05 22:35:05 -0700

    Betty free me na

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,38 +17,34 @@ void times_table(void)
 		for (col = 0; col <= 9; col++)
 		{num = row * col;
 			if (col == 0)
-			{_putchar(num + '0');
+			{	_putchar(num + '0');
 				_putchar(',');
 
-			}
-			else if (col == 9 && num  < 10)
-			{_putchar(' ');
+			} else if (col == 9 && num  < 10)
+			{	_putchar(' ');
 				_putchar(' ');
 				_putchar(num + '0');
-			}
-			else if (col == 9 && num >= 10)
-			{dig1 = num / 10;
+			} else if (col == 9 && num >= 10)
+			{	dig1 = num / 10;
 				dig2 = num % 10;
 				_putchar(' ');
 				_putchar(dig1 + '0');
 				_putchar(dig2 + '0');
 
-			}
-			else if (col != 9 && num >= 10)
-			{dig1 = num / 10;
+			} else if (col != 9 && num >= 10)
+			{	dig1 = num / 10;
 				dig2 = num % 10;
 				_putchar(' ');
 				_putchar(dig1 + '0');
 				_putchar(dig2 + '0');
 				_putchar(',');
 
-			}
-			else
-			{_putchar(' ');
+			} else
+			{	_putchar(' ');
 				_putchar(' ');
 				_putchar(num + '0');
 				_putchar(',');
 
 			}
-		}_putchar('\n');
+		}	_putchar('\n');
 	}}
