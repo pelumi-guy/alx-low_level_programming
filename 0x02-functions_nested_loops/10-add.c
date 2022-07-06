ef0c62f9eb6d382436018afd9e9f98b1d7be85f0
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-05 22:40:30 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - a function that adds two integers 
+ * and returns the result.
+ *
+ * 
+ * Return: Result
+ */
+
+int add(int a, int b) 
+{
+	int sum;
+
+	sum = a + b;
+
+        return (sum);
+}
