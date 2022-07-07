15c11a3d33fc91996383a71d0ce76fd0903abce6
pelumi-guy <101118086+pelumi-guy@users.noreply.github.com>
2022-07-07 01:18:13 +0100

    Task 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,39 +5,41 @@
   */
 int main(void)
 {
-    int i;
-    unsigned long prev, next, fib;
-    unsigned long m, n, p, carry;
+ int i;
+ unsigned long prev, next, fib;
+ unsigned long m, n, p, carry;
 
-    prev = 0;
-    next = 1;
-    for (i = 1; i <= 91; i++)
-    {
-        fib = prev + next;
-        prev = next;
-        next = fib;
-        printf("%lu, ", fib);
-    }
-    m = prev % 1000;
-    prev = prev / 1000;
-    n = next % 1000;
-    next = next / 1000;
-    for (i = 91; i <= 98; i++)
-    {
-        carry = (m + n) / 1000;
-        p = (m + n) - carry * 1000;
-        fib = (prev + next) + carry;
-        m = n;
-        n = p;
-        prev = next;
-        next = fib;
-        if (p >= 100)
-            printf("%lu%lu", fib, p);
-        else
-            printf("%lu0%lu", fib, p);
-        if (i != 98)
-            printf(", ");
-}
-    putchar('\n');
-    return (0);
+	i = 0;
+	prev = 0;
+	next = 1;
+	for (i = 1; i <= 91; i++)
+	{
+		fib = prev + next;
+		prev = next;
+		next = fib;
+		printf("%lu, ", fib);
+	}
+	m = prev % 1000;
+	prev = prev / 1000;
+	n = next % 1000;
+	next = next / 1000;
+	while (i <= 98)
+	{
+		carry = (m + n) / 1000;
+		p = (m + n) - carry * 1000;
+		fib = (prev + next) + carry;
+		m = n;
+		n = p;
+		prev = next;
+		next = fib;
+		if (p >= 100)
+			printf("%lu%lu", fib, p);
+		else
+			printf("%lu0%lu", fib, p);
+		if (i != 98)
+			printf(", ");
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
