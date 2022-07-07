a9eaecfb94a380755278e09dae50b55c2eed53d5
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-07 16:40:28 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+/**
+ * print_numbers - a function that prints the numbers,
+ * from 0 to 9, followed by a new line.
+ * Return: Nothing
+ */
+
+#include "main.h"
+
+int print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
