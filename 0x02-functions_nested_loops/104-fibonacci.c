c78da6d4c4a9749f4d92e664a442b20dd20af2f7
pelumi-guy <101118086+pelumi-guy@users.noreply.github.com>
2022-07-07 01:27:18 +0100

    Task 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,40 +5,40 @@
   */
 int main(void)
 {
-	int i;
-	unsigned long prev, next, fib;
+	int count;
+	unsigned long i, j, k;
 	unsigned long m, n, p, carry;
 
+	count = 0;
 	i = 0;
-	prev = 0;
-	next = 1;
-	for (i = 1; i <= 91; i++)
+	j = 1;
+	for (count = 1; count <= 91; count++)
 	{
-		fib = prev + next;
-		prev = next;
-		next = fib;
-		printf("%lu, ", fib);
+		k = i + j;
+		i = j;
+		j = k;
+		printf("%lu, ", k);
 	}
-	m = prev % 1000;
-	prev = prev / 1000;
-	n = next % 1000;
-	next = next / 1000;
-	while (i <= 98)
+	m = i % 1000;
+	i = i / 1000;
+	n = j % 1000;
+	j = j / 1000;
+	while (count <= 98)
 	{
 		carry = (m + n) / 1000;
 		p = (m + n) - carry * 1000;
-		fib = (prev + next) + carry;
+		k = (i + j) + carry;
 		m = n;
 		n = p;
-		prev = next;
-		next = fib;
+		i = j;
+		j = k;
 		if (p >= 100)
-			printf("%lu%lu", fib, p);
+			printf("%lu%lu", k, p);
 		else
-			printf("%lu0%lu", fib, p);
-		if (i != 98)
+			printf("%lu0%lu", k, p);
+		if (count != 98)
 			printf(", ");
-		i++;
+		count++;
 	}
 	putchar('\n');
 	return (0);
