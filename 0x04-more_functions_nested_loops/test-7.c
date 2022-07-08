b3da4825ebfe558033f5c7a195f8b746bbba1d89
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-07 18:03:52 -0700

    Task 8

---
+++ 0x04-more_functions_nested_loops/test-7.c
@@ -0,0 +1,31 @@
+/**
+ * print_diagonal - a function that draws
+ * a diagonal line on the terminal
+ * @n: number of times to print
+ * Return: Nothing
+ */
+
+#include "main.h"
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 1; i <= n; i++)
+	{
+		if (n <= 0)
+			break;
+
+		if (i > 1)
+		{
+			for (j = 1; j <= (i - 1); j++)
+				_putchar(' ');
+		}
+
+		_putchar('\\');
+
+		if (i != n)
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
