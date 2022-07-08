d6ced68ffca0d24e8adfb80db0d676855ac11718
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-07 18:09:00 -0700

    Task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,10 +13,16 @@ void print_square(int size)
 
 	for (i = 1; i <= size; i++)
 	{
+		if (size <= 0)
+			break;
+
 		for (j = 1; j <= size; j++)
 		{
 			_putchar('#');
 		}
-		_putchar('\n');
+
+		if (i != size)
+			_putchar('\n');
 	}
+	_putchar('\n');
 }
