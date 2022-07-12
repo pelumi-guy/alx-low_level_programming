4ab88db68a1478b159101009bbe08853cc7d141f
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-12 15:47:10 -0700

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,8 +13,9 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; *(str + i); i += 2)
-		_putchar(*(str + i));
+	for (i = 0; *(str + i); i++)
+		if ((i % 2) == 0)
+			_putchar(*(str + i));
 
 	_putchar('\n');
 }
