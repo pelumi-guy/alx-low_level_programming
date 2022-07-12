38591fc661687800889c6f1bf5f3d4a1bdf22047
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-12 15:24:00 -0700

    TAsk 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,10 +12,12 @@
 void rev_string(char *s)
 {
 	int i, j, l = 0;
-	char d[];
+
 
 	for (i = 0; *(s + i); i++)
 		l++;
+
+	char d[l];
 
 	for (i = (l - 1), j = 0; i >= 0; i--, j++)
 		d[j] = (*(s + i));
