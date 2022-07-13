61af2c2a73f6358e8d0594bbcf7591ec88e74e19
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-12 17:15:23 -0700

    Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,9 +14,9 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0, n--; n >= 0; i++, n--)
 	{
-		if (i != (n - 1))
+		if (n > 0)
 			printf("%d,", a[i]);
 		else
 			_putchar(a[i]);
