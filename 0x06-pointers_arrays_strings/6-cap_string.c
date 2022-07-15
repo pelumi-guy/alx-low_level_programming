24cd4719fc6f953ee10e35ca2efa76f8d6fb8ca9
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-14 18:15:06 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,14 +10,27 @@
 
 char *cap_string(char *s)
 {
-	int i, diff;
+	int i, j, diff;
+	char sym[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
 
 	diff = 'a' - 'A';
 
 	for (i = 0; s[i]; i++)
+	{
 		if ('a' <= s[i] && s[i] <= 'z')
 			s[i] = s[i] - diff;
 
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == sym[j])
+			{
+				if ('a' <= s[i + 1] && s[i + 1] <= 'z')
+					s[i + 1] = s[i + 1] - diff;
+			}
+		}
+
+	}
 
 	return (s);
 }
