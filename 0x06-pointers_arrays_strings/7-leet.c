8d617f2d6b8c7bccc003e2e16801b75d8f1c051c
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-14 18:34:42 -0700

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+/**
+ * leet - a function that encodes a string into 1337
+ * Description:
+ * @s: string to be encoded
+ * Return: Encoded string.
+ */
+
+#include "main.h"
+
+char *leet(char *s)
+{
+	int i, j;
+	char *strLow = "aeotl";
+	char *strUp = "AEOTL";
+	char *strNum = "43071"
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; j < 5; j++)
+			if (s[i] == strLow[j] || s[i] == strup[j])
+				s[i] = strNum[j];
+	}
+
+	return (s);
+}
