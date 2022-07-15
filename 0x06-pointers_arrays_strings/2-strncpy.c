89aa8499b22359854cf7cd8529de4427da15d68e
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-14 17:12:20 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,8 +16,9 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; src[i] != '\0' && i < n; i++)
 		dest[i] = src[i];
-
-	dest[i] = '\0';
+	
+	for (; i < n; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
