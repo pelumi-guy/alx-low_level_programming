745a42c44acd385255ca1cb57db3b42c269fe8f4
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-18 18:45:22 -0700

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,31 +1,31 @@
 /**
- * char *_strchr - a function that locates a character in a string.
- * Description:
- * @s: string to be searched
- * @c: character to be found
- * Return: Always pointer to first occurence of char or NULL
- */
+* char *_strchr - a function that locates a character in a string.
+* Description:
+* @s: string to be searched
+* @c: character to be found
+* Return: Always pointer to first occurence of char or NULL
+*/
 
 #include "main.h"
 
 char *_strchr(char *s, char c)
 {
-	char find;
-	int i, l = 0;
+char *find;
+int i, l = 0;
 
-	for (i = 0; *(s + i); i++)
-		l++;
+for (i = 0; *(s + i); i++)
+l++;
 
-	for (i = 0; i < l; i++)
-	{
-		find = *(s + i);
+for (i = 0; i < l; i++)
+{
+find = &s[i];
 
-		if (find == c)
-			break;
-	}
+if (*find == c)
+break;
+}
 
-	if (find == c)
-		return (find);
-	else
-		return (NULL);
+if (*find == c)
+return (find);
+else
+return (NULL);
 }
--- 0x07-pointers_arrays_strings/test.c
+++ 0x07-pointers_arrays_strings/test.c
@@ -6,6 +6,29 @@
  *
  * Return: Always 0.
  */
+char *_strchr(char *s, char c)
+{
+        char *find;
+        int i, l = 0;
+
+        for (i = 0; *(s + i); i++)
+                l++;
+
+        for (i = 0; i < l; i++)
+        {
+                find = &s[i];
+
+                if (*find == c)
+                        break;
+        }
+
+        if (*find == c)
+                return (find);
+        else
+                return (NULL);
+}
+
+
 int main(void)
 {
     char *s = "hello";
