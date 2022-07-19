13d9a5ba9f67ed812cb5ff631d7c9f187796e830
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-18 20:24:38 -0700

    Tassk 100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+/**
+ * void set_string - a function that sets the
+ * value of a pointer to a char.
+ * Description:
+ * @s: pointer to string
+ * @to: string pointer
+ * Return:
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
