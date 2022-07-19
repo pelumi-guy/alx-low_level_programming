e94bffb9cc6547e13ab6e0b1e5efd8cd091d732f
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-18 18:59:49 -0700

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,27 +8,26 @@
  * Return: the number of bytes in the initial segment
  * of s which consist only of bytes from accept
  */
+
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j, f, flag;
+unsigned int i, j, counter;
 
-	f = 0;
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		flag = 0;
-		for (j = 0; accept[j] != '\0'; j++)
-		{
-			if (s[i] == accept[j])
-			{
-			f++;
-			flag = 1;
-			}
-			if (flag == 0)
-			{
-				return (f);
-			}
-		}
-
-	return (0);
-	}
+counter = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (accept[j] == s[i])
+{
+counter++;
+break;
+}
+}
+if (accept[j] != s[i])
+{
+break;
+}
+}
+return (counter);
 }
