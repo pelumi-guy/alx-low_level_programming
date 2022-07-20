145c0878455971cb4f7ee76ba9f125e47e1f9631
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-20 11:52:05 -0700

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+/**
+ * _strlen_recursion - a function that returns the length of a string
+ * Description:
+ * @s: string to be printed
+ * Return:
+ */
+
+#include "main.h"
+
+int _strlen_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+
+}
