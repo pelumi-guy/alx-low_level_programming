c03b7114976e9fdf0c3223062e32291012e4d287
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-22 09:24:43 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+/**
+ * main - a program that prints its name, followed by a new line.k
+ * Description:
+ * @argc: number of arguments to func main
+ * @argv: array of argument strings
+ * Return: Always 0 (Success).
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
