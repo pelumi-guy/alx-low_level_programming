010b2a98b349651d4b502cf8bff033990aa147e2
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-22 09:46:25 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+/**
+ * main - a program that prints the number of arguments passed into it.
+ * Description:
+ * @argc: number of arguments to func main
+ * @argv: array of argument strings
+ * Return: 0 - Always succes.
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
