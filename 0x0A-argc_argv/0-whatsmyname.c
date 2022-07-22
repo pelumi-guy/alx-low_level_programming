31e9351338dd237ade8e6b0a4cf9214031190429
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-22 09:43:15 -0700

    Redid Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,13 +3,15 @@
  * Description:
  * @argc: number of arguments to func main
  * @argv: array of argument strings
- * Return:
+ * Return: 0 - Always succes.
  */
 
 #include <stdio.h>
 #include "main.h"
 
-void main(int argc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
+
+	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -4,6 +4,6 @@
 #include <stdio.h>
 
 int _putchar(char c);
-void main (int argc, char *argv[]);
+
 
 #endif /* MAIN_H */
