06c34787a5d2ec758f6bf5945cc8d56f1df65215
Pelumi <pelumi.olalekan.g@gmail.com>
2022-07-25 19:04:17 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,12 +15,12 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *arr;
-	
+
 	if (size == 0)
-		return NULL;
+		return (NULL);
 
 	arr = calloc(size, sizeof(c));
-	
+
 	for (i = 0; i < size; i++)
 		arr[i] = c;
 
