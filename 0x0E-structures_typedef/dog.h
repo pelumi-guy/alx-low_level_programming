19b9a138a8c97e044bb5d1be0bce26b40003b821
Pelumi <pelumi.olalekan.g@gmail.com>
2022-08-02 14:24:12 -0700

    Task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DOG_H
+#define DOG_H
+#pragma once
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,9 +22,9 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dot_t;
+typedef struct dog dog_t;
 /* dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d); */
 
 
-#endif /* MAIN_H */
+#endif /* DOG_H */
