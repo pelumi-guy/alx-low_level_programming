9e691eeaeb9e5d06a5efa1a24fabf66ffbc9cc1f
Pelumi <pelumi.olalekan.g@gmail.com>
2022-08-02 15:50:50 -0700

    Task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+/**
+ * free_dog - a function that frees dogs.
+ * Description:
+ * @d: dog to be freed
+ * Return: void
+ */
+
+#include <stdio.h>
+#include "dog.h"
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
