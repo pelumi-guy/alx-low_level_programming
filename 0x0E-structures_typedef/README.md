f7a561cf519e3b0f3e18ce0e0262a982b7351aec
Pelumi <pelumi.olalekan.g@gmail.com>
2022-08-02 12:47:59 -0700

    TAsk 0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,6 @@
+# 0x0E. C - Structures, typedef0x0E. C - Structures, typedef
+### 0. Poppy
+- Define a new type struct dog with the following elements:
+	- name, type = char *
+	- age, type = float
+	- owner, type = char *
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+/**
+ * struct dog - A simple structure describing a dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description:
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* MAIN_H */
