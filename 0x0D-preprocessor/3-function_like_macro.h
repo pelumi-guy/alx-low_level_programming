bae6a6e30295fe835b0b4dd532c89f946572a10c
Pelumi <pelumi.olalekan.g@gmail.com>
2022-08-02 20:37:03 -0700

    Task 3

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+#define ABS(X) (X < 0 ? X * -1 : X)
+
+
+#endif /* FUNCTION_LIKE_MACRO_H */
