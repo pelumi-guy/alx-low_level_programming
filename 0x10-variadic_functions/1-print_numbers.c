f4ec40b0678ee10ed84404eb47c0949b26ebfe3c
Pelumi <pelumi.olalekan.g@gmail.com>
2022-08-04 17:53:31 -0700

    Task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,18 +13,18 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list arglist;
 
-	if (separator == NULL)
-		return;
-
 	va_start(arglist, n);
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(arglist, int));
 
-		if (i != (n - 1))
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
-	va_end(arglist);
 
 	printf("\n");
+
+	va_end(arglist);
+
+
 }
