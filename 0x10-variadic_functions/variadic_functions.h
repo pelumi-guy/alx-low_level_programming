5c752a4cbc691e6d56f8917569028e81c70aa553
Pelumi <pelumi.olalekan.g@gmail.com>
2022-08-04 18:40:22 -0700

    Task 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -10,5 +10,15 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
+/**
+ * struct printfunc - pointer to a list of types of function arguments
+ * @ch: input
+ * @func: output function
+ **/
+struct printfunc
+{
+	char *ch;
+	void (*func)(va_list arg);
+};
+typedef struct printfunc printer_t;
 #endif /* VARIADIC_fUNCTIONS_H */
