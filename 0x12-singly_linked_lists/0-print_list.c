08e9e38b78728a28ccb7c4101cdd7849ba9d1a41
Pelumi <pelumi.olalekan.g@gmail.com>
2022-08-11 22:05:49 -0700

    Redid Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,16 +17,11 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-			h = h->next;
-			nodes++;
-			continue;
-		}
+		else
+			printf("[%d] %s\n", h->len, h->str);
 
-		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		nodes++;
 	}
 	return (nodes);
 }
