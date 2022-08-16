c871c9c103cb5bb3faae0dce57e2eb12bee64450
Pelumi <pelumi.olalekan.g@gmail.com>
2022-08-15 20:48:32 -0700

    Task 5

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,7 +13,7 @@ size_t print_listint(const listint_t *h)
 
 	if (h == NULL)
 	{
-		printf("(nil)\n");
+		/* printf("(nil)\n"); */
 		return (0);
 	}
 
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,14 +9,15 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *temp, *curr;
 
 	if (*head == NULL)
 		return;
 
-	while ((temp = *head) != NULL)
+	curr = *head;
+	while ((temp = curr) != NULL)
 	{
-		*head = (*head)->next;
+		curr = curr->next;
 		free(temp);
 	}
 	*head = NULL;
