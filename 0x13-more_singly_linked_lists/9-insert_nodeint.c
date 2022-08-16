5e1e9aee2dea4ccf9c839f0c8f90798c35359066
Pelumi <pelumi.olalekan.g@gmail.com>
2022-08-16 09:42:50 -0700

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,6 +22,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	newNode->n = n;
 
+	if (idx == 0)
+	{
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
+	}
+
 	for (i = 0; temp != NULL && i < (idx - 1); i++)
 		temp = temp->next;
 
