0067e68276a8d3b6b96921910da3af45adaaeb30
Pelumi <pelumi.olalekan.g@gmail.com>
2022-08-16 16:33:11 -0700

    Task 100

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -12,7 +12,7 @@ listint_t *reverse_listint(listint_t **head)
 	listint_t *prevNode = NULL;
 	listint_t *nextNode = NULL;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (0);
 
 	while (*head)
