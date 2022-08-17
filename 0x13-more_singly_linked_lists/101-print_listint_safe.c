a0f14b2029cdd387dcc31030f36f508d3c1fb9eb
Pelumi <pelumi.olalekan.g@gmail.com>
2022-08-16 18:11:09 -0700

    Task 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,27 @@
 #include "lists.h"
 
 /**
+ * is_visited - check if a node is visited
+ * @node: pointer to node
+ * @visited: list of visited
+ * @count: length of visited
+ *
+ * Return: 1 if is visited and 0 otherwise
+ */
+int is_visited(const listint_t *node, const listint_t **visited, int count)
+{
+	int i = 0;
+
+	while (i < count)
+	{
+		if (node == visited[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+/**
  * print_listint_safe - prints a list even with loop
  * @head: pointer to head
  *
@@ -23,25 +44,4 @@ size_t print_listint_safe(const listint_t *head)
 		head = head->next;
 	}
 	return (count);
-}
-
-/**
- * is_visited - check if a node is visited
- * @node: pointer to node
- * @visited: list of visited
- * @count: length of visited
- *
- * Return: 1 if is visited and 0 otherwise
- */
-int is_visited(const listint_t *node, const listint_t **visited, int count)
-{
-	int i = 0;
-
-	while (i < count)
-	{
-		if (node == visited[i])
-			return (1);
-		i++;
-	}
-	return (0);
 }
