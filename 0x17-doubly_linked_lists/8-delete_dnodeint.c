001b6a37f2e1428eb51e248469e7359ce386c523
Pelumi <pelumi.olalekan.g@gmail.com>
2022-09-30 05:17:45 +0000

    task 8 updated

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -22,7 +22,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (index == 0)
 	{
-		if (*head)
+		if (*head != NULL)
 		{
 			*head = (*head)->next;
 			if (*head != NULL)
