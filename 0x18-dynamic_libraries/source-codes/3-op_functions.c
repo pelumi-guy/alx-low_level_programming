01d84a6364e520d3e42f4bebca470c60f01c9ea9
Pelumi <pelumi.olalekan.g@gmail.com>
2022-10-04 03:51:34 +0000

    task 100

---
+++ 0x18-dynamic_libraries/source-codes/3-op_functions.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - function to add 2 numbers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
+ **/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - function to add 2 numbers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
+ **/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - function to add 2 numbers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
+ **/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - function to add 2 numbers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
+ **/
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - function to add 2 numbers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
+ **/
+int mod(int a, int b)
+{
+	return (a % b);
+}
