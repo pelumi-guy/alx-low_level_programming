c447260b118c6148b5fb175fbe45a7c2d85538b3
Pelumi <pelumi.olalekan.g@gmail.com>
2022-10-21 05:13:49 +0000

    task 0 updated

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,13 +10,20 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht = malloc(sizeof(hash_table_t *));
-
+	hash_table_t *ht = malloc(sizeof(hash_table_t));
+	unsigned long int i;
+	
 	if (!ht)
 		return (NULL);
 
 	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
+
+	if ((ht->array) == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		(ht->array)[i] = NULL;
 
 	return (ht);
 }
