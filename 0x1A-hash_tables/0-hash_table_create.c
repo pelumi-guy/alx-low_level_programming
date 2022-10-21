71b4ca4fef144e7385bd96e5b6d9d37cd8d684eb
Pelumi <pelumi.olalekan.g@gmail.com>
2022-10-21 05:07:00 +0000

    task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - a function that creates a hash table.
+ * @size: is the size of the array bucket for the hash table
+ * Description:
+ * Return:  pointer to the newly created hash table
+ *		or NULL if it fails
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht = malloc(sizeof(hash_table_t *));
+
+	if (!ht)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+
+	return (ht);
+}
