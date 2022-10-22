9a7592ffc14dff20215730f040cd97c6b20eb779
Pelumi <pelumi.olalekan.g@gmail.com>
2022-10-22 03:46:17 +0000

    task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - a function that deletes a hash table.
+ * Description:
+ * @ht: hash table to delete
+ * Return:
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *slot, *temp;
+
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] == NULL)
+			continue;
+		slot = ht->array[i];
+		while(slot)
+		{
+			temp = slot->next;
+			free(slot->key);
+			free(slot->value);
+			free(slot);
+			slot = temp;
+		}
+	}
+	free(ht->array);
+	free(ht);
+
+}
